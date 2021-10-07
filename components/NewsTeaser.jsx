e00289e844e91f5030050313a97d5ef6738272fc
Michael Zeta <michael@a2zeta.com>
2021-10-06 17:57:50 -0700

    Adds NPM packages for masonry layout of news teaser on homepage

--- components/NewsTeaser.jsx
+++ components/NewsTeaser.jsx
@@ -1,68 +1,127 @@
 import selectors from '../styles/NewsTeaser.module.scss'
+import Masonry, { ResponsiveMasonry } from 'react-responsive-masonry'
+//? This component also requires the package 'prop-types' to function correctly.
+//? https://www.npmjs.com/package/react-responsive-masonry
+//? https://stackoverflow.com/questions/43357501/react-prop-types-error
 
 const NewsTeaser = () => {
   return (
     <section className='container' id={selectors.news_teaser}>
       <h2>Latest Updates</h2>
       <hr />
-      <div>
-        <article>
-          <img src='https://source.unsplash.com/random/300x300' />
-          <h5>New Video - Jan 12 2021</h5>
-          <h3>Interstellar Love Song – A Masterpiece in the Making</h3>
-          <p>
-            Article Excerpt lorem ipsum dolor sit amet, consectetur adipiscing
-            elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-            aliqua. Llorem ipsum dolor sit amet, consectetur adipiscing elit,
-            sed do eiusmod tempor incididunt.
-          </p>
-        </article>
-      </div>
+      <ResponsiveMasonry
+        columnsCountBreakPoints={{ 400: 1, 800: 2, 1000: 3, 1500: 4 }}
+      >
+        <Masonry gutter='1em'>
+          <article>
+            <img src='https://source.unsplash.com/random/160x90' />
+            <h5>New Video - Jan 12 2021</h5>
+            <h3>Interstellar Love Song – A Masterpiece in the Making</h3>
+            <p>
+              Article Excerpt lorem ipsum dolor sit amet, consectetur adipiscing
+              elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+              aliqua. Llorem ipsum dolor sit amet, consectetur adipiscing elit,
+              sed do eiusmod tempor incididunt.
+            </p>
+          </article>
 
-      <div>
-        <article>
-          <img src='https://source.unsplash.com/random/300x150' />
+          <article>
+            <img src='https://source.unsplash.com/random/300x300' />
+            <h5>New Song - Dec 05 2020</h5>
+            <h3>Kelly Come Home</h3>
+            <p>
+              My fiancee sometimes leaves on these long trips to visit her
+              family in the bay. Sometimes she's gone for days, so I wrote this
+              song so she'd know how helpless I am without her.
+            </p>
+          </article>
 
-          <h5>New Song - Dec 05 2020</h5>
-          <h3>Kelly Come Home</h3>
-          <p>
-            My fiancee sometimes leaves on these long trips to visit her family
-            in the bay. Sometimes she's gone for days, so I wrote this song so
-            she'd know how helpless I am without her.
-          </p>
-        </article>
-      </div>
-      <div>
-        <article>
-          <img src='https://source.unsplash.com/random/300x660' />
-          <h5>Upcoming Show - Nov 11 2020</h5>
-          <h3>Michael's Awesome Show</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
-            provident earum facere quo iste alias? Eaque similique ipsa fugit
-            corrupti illo culpa voluptatum mollitia, repellat quisquam id
-            officiis optio nihil magnam illum ad a fuga minus animi beatae
-            accusantium odit error consectetur nam doloribus. Sapiente vitae,
-            error modi quo libero repudiandae nulla facere quia inventore est
-            sed. Aliquid, unde quibusdam?
-          </p>
-        </article>
-      </div>
-      <div>
-        <article>
-          <img src='https://source.unsplash.com/random/300x900' />
+          <article>
+            <img src='https://source.unsplash.com/random/160x200' />
+            <h5>Upcoming Show - Nov 11 2020</h5>
+            <h3>Michael's Awesome Show</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+              provident earum facere quo iste alias? Eaque similique ipsa fugit
+              corrupti illo culpa voluptatum mollitia, repellat quisquam id
+              officiis optio nihil magnam illum ad a fuga minus animi beatae
+              accusantium odit error consectetur nam doloribus. Sapiente vitae,
+              error modi quo libero repudiandae nulla facere quia inventore est
+              sed. Aliquid, unde quibusdam?
+            </p>
+          </article>
 
-          <h5>New Video - Oct 23 2020</h5>
-          <h3>Dream Girl</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
-            delectus esse nulla ea. Magnam natus architecto voluptatum odit ut
-            aspernatur error vel alias quam dolorem quaerat similique cum,
-            cumque aliquid reiciendis neque dolores quia eum. Modi neque
-            quisquam iusto fuga minus in cum esse reiciendis.
-          </p>
-        </article>
-      </div>
+          <article>
+            <img src='https://source.unsplash.com/random/160x90' />
+
+            <h5>New Video - Oct 23 2020</h5>
+            <h3>Dream Girl</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias
+              delectus esse nulla ea. Magnam natus architecto voluptatum odit ut
+              aspernatur error vel alias quam dolorem quaerat similique cum,
+              cumque aliquid reiciendis neque dolores quia eum. Modi neque
+              quisquam iusto fuga minus in cum esse reiciendis.
+            </p>
+          </article>
+          <article>
+            <img src='https://source.unsplash.com/random/160x200' />
+            <h5>Upcoming Show - Nov 11 2020</h5>
+            <h3>Michael's Awesome Show</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+              provident earum facere quo iste alias? Eaque similique ipsa fugit
+              corrupti illo culpa voluptatum mollitia, repellat quisquam id
+              officiis optio nihil magnam illum ad a fuga minus animi beatae
+              accusantium odit error consectetur nam doloribus. Sapiente vitae,
+              error modi quo libero repudiandae nulla facere quia inventore est
+              sed. Aliquid, unde quibusdam?
+            </p>
+          </article>
+
+          <article>
+            <img src='https://source.unsplash.com/random/160x90' />
+            <h5>New Video - Jan 12 2021</h5>
+            <h3>Interstellar Love Song – A Masterpiece in the Making</h3>
+            <p>
+              Article Excerpt lorem ipsum dolor sit amet, consectetur adipiscing
+              elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+              aliqua. Llorem ipsum dolor sit amet, consectetur adipiscing elit,
+              sed do eiusmod tempor incididunt.
+            </p>
+          </article>
+
+          <article>
+            <img src='https://source.unsplash.com/random/160x200' />
+            <h5>Upcoming Show - Nov 11 2020</h5>
+            <h3>Michael's Awesome Show</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+              provident earum facere quo iste alias? Eaque similique ipsa fugit
+              corrupti illo culpa voluptatum mollitia, repellat quisquam id
+              officiis optio nihil magnam illum ad a fuga minus animi beatae
+              accusantium odit error consectetur nam doloribus. Sapiente vitae,
+              error modi quo libero repudiandae nulla facere quia inventore est
+              sed. Aliquid, unde quibusdam?
+            </p>
+          </article>
+
+          <article>
+            <img src='https://source.unsplash.com/random/160x200' />
+            <h5>Upcoming Show - Nov 11 2020</h5>
+            <h3>Michael's Awesome Show</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+              provident earum facere quo iste alias? Eaque similique ipsa fugit
+              corrupti illo culpa voluptatum mollitia, repellat quisquam id
+              officiis optio nihil magnam illum ad a fuga minus animi beatae
+              accusantium odit error consectetur nam doloribus. Sapiente vitae,
+              error modi quo libero repudiandae nulla facere quia inventore est
+              sed. Aliquid, unde quibusdam?
+            </p>
+          </article>
+        </Masonry>
+      </ResponsiveMasonry>
     </section>
   )
 }
--- pages/merch.jsx
+++ pages/merch.jsx
@@ -1,12 +1,56 @@
+import { useState, useEffect } from 'react'
+
 import Nav from '../components/Nav'
 
 const merch = () => {
-  return (
-    <>
-      <Nav />
-      <h1>Hello From /Merch</h1>
-    </>
-  )
+  const [error, setError] = useState(null)
+  const [isLoaded, setIsLoaded] = useState(false)
+  const [items, setItems] = useState({})
+
+  // Note: the empty deps array [] means
+  // this useEffect will run once
+  // similar to componentDidMount()
+  useEffect(() => {
+    fetch(
+      'http://musicbrainz.org/ws/2/artist/5b11f4ce-a62d-471e-81fc-a69a8278c7da?fmt=json'
+    )
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true)
+          // console.log(result)
+          setItems(result.name)
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          setIsLoaded(true)
+          setError(error)
+        }
+      )
+  }, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>
+  } else if (!isLoaded) {
+    return <div>Loading...</div>
+  } else {
+    return (
+      <>
+        <Nav />
+        <h1>Hello From /Merch</h1>
+        <ul>
+          {{ items }}
+          {/* {items.map((item) => (
+            <li key={item.id}>
+              {item.name} {item.price}
+            </li>
+          ))} */}
+        </ul>
+      </>
+    )
+  }
 }
 
 export default merch
